a5f2cfb47f1e3aff7610fde0b7dfc12d4798b063
Juan Manuel Lallana <jlallana@jusbaires.gob.ar>
2017-02-24 20:51:34 +0000

    Integracion con google analytics

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@
 (function() {
     "use strict";
     
+    //Google analytics
+    if(window.location.hostname.split('.').slice(1).join(".") == "github.io") {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        
+        ga('create', 'UA-92549592-1', 'auto');
+        ga('send', 'pageview');
+    }
+    
+    
     window.addEventListener("popstate", function() {
         if(window.location.hash == "") {
             document.body.className = "";
